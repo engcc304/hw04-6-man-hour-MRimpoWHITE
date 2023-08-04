08920aa08ad807edeb4d6d6181458f24de6e326e
MRimpoWHITE <phichitphu2015@gmail.com>
2023-08-05 04:14:19 +0700

    123

--- Homework/HW04/hw04-6-man-hour.cpp
+++ Homework/HW04/hw04-6-man-hour.cpp
@@ -28,5 +28,27 @@
 */
 
 
+#include<stdio.h>
+
+int main(){
+
+    int Eid ;
+    float WorkHr , S;
+
+    printf("Input the Employees ID(Max. 10 chars): \n");
+    scanf("%d", &Eid);
+
+    printf("Input the working hrs: \n");
+    scanf("%f" , &WorkHr);
+
+    printf("Salary amount/hr: \n");
+    scanf("%f" , &S);
+
+    printf("Expected Output:\n");
+    printf("Employees ID = %d\n", Eid);
+    printf("Salary = U$ %0.2f", WorkHr * S);
+
+    return 0 ;
+}
 
 
