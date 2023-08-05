e0dc171f223e3561861229dc57458f8238b8a3b5
MRimpoWHITE <phichitphu2015@gmail.com>
2023-08-05 15:13:08 +0700

    123

--- Homework/HW04/hw04-6-man-hour.cpp
+++ Homework/HW04/hw04-6-man-hour.cpp
@@ -27,16 +27,80 @@
         Salary = U$ 374,000.00
 */
 
-#include <stdio.h>
-#include <locale.h>
+// #include <stdio.h>
+// #include <locale.h>
 
-int main()
-{
+// int main()
+// {
+//     int i;
+//     char Eidstr[11];
+//     float WorkHr, S;
+//     // setlocale(LC_NUMERIC, "");
+
+//     printf("Input the Employees ID(Max. 10 chars): \n");
+//     scanf("%10s", Eidstr);
+
+//     printf("Input the working hrs: \n");
+//     scanf("%f", &WorkHr);
+
+//     printf("Salary amount/hr: \n");
+//     scanf("%f", &S);
+
+//     float salary = WorkHr * S;
+
+//     printf("Expected Output:\n");
+//     printf("Employees ID = %s\n", Eidstr);
+    
+
+//     // if( WorkHr == 11){
+//     //     printf("Salary = U$ 374,000.00");
+//     // }else{
+//     //     printf("Salary = U$ 120,000.00");
+//     // }
+
+//     printf("Salary = U$ %'0.2f\n", salary);
+
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+#include <stdio.h>
+#include <string.h>
+
+void addCommasToFloat(float value, char *formattedValue) {
+    // Assuming the formattedValue will fit within 20 characters
+    char temp[20];
+    sprintf(temp, "%.2f", value);
+
+    int length = strlen(temp);
+    int commas = (length - 4) / 3; // Calculate how many commas are needed
+
+    int j = 0;
+    for (int i = 0; i < length; i++) {
+        if (temp[i] == '.') {
+            formattedValue[j++] = '.';
+            continue;
+        }
+
+        formattedValue[j++] = temp[i];
+        if ((length - i - 1) % 3 == 0 && (length - i - 1) != 0) {
+            formattedValue[j++] = ',';
+        }
+    }
+    formattedValue[j] = '\0';
+}
+
+int main() {
     int i;
     char Eidstr[11];
     float WorkHr, S;
-    setlocale(LC_NUMERIC, "");
-    
+
     printf("Input the Employees ID(Max. 10 chars): \n");
     scanf("%10s", Eidstr);
 
@@ -48,17 +112,14 @@ int main()
 
     float salary = WorkHr * S;
 
+    char formattedSalary[20];
+    addCommasToFloat(salary, formattedSalary);
+
     printf("Expected Output:\n");
     printf("Employees ID = %s\n", Eidstr);
-    
-
-    // if( WorkHr == 11){
-    //     printf("Salary = U$ 374,000.00");
-    // }else{
-    //     printf("Salary = U$ 120,000.00");
-    // }
-
-    printf("Salary = U$ %'3.2f\n", salary);
+    printf("Salary = U$ %s\n", formattedSalary);
 
     return 0;
 }
+
+
