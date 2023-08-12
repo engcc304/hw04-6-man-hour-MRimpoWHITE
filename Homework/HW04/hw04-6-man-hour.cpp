eccb4de72039b870b9f30fdbc74667a89330cb15
MRimpoWHITE <phichitphu2015@gmail.com>
2023-08-12 13:38:53 +0700

    Thanks

--- Homework/HW04/hw04-6-man-hour.cpp
+++ Homework/HW04/hw04-6-man-hour.cpp
@@ -28,13 +28,12 @@
 */
 
 #include <stdio.h>
-#include <locale.h>
 
 int main()
 {
     char Eidstr[11];
     float WorkHr, S;
-    setlocale(LC_NUMERIC, "");
+    int i;
 
     printf("Input the Employees ID(Max. 10 chars): \n");
     scanf("%10s", Eidstr);
@@ -50,9 +49,37 @@ int main()
     printf("Expected Output:\n");
     printf("Employees ID = %s\n", Eidstr);
 
-    
+    int temp = 1, comma_count = 0;
+    int salary_int = (int)salary; // Extracting the integer part of the salary
 
-    printf("Salary = U$ %'0.2f\n", salary);
+    // Count the number of digits in the integer part of the salary
+    int temp_salary = salary_int;
+    while (temp_salary > 0)
+    {
+        temp_salary /= 10;
+        comma_count++;
+    }
+
+    for (i = 1; i < comma_count; i++)
+    {
+        temp *= 10;
+    }
+    printf("Salary = U$ ");
+    i = 0; // Resetting the loop counter
+    while (temp > 0)
+    {
+        printf("%d", salary_int / temp);
+        salary_int %= temp;
+        temp /= 10;
+        i++;
+
+        if (i < comma_count && i % 3 == 0)
+        {
+            printf(",");
+        }
+    }
+    printf(".00");
 
     return 0;
 }
+
